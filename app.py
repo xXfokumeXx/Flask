10f05def36349adf3fcefd884e5f30db177e80ab
xXfokumeXx <radek.cerny847@gmail.com>
2025-07-10 20:17:51 +0200

    added new route /fancy for testing

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/fancy")
+def hello_world_fancy():
+    return """
+        <html>
+        <body>
+
+        <h1>Greetings!</h1>
+        <p>"Hello, World"!</p>
+        
+        </body>
+        </html>
+    """
